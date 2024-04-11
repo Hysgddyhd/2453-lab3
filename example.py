02e8ce1c46b93f7a36b42006eefbceb57c794f52
Hysgddyhd <129603595+Hysgddyhd@users.noreply.github.com>
2024-04-11 16:20:36 +0800

    update example.py

--- example.py
+++ example.py
@@ -1,6 +1,69 @@
-from sklearn.datasets import make_blobs
+# example of generating a small classification dataset
+from statistics import mean
 
-X,y=make_blobs(n_samples=100,n_features=2,centers=2,random_state=1)
-print(X.shape,y.shape)
+from numpy import std
+from numpy.linalg import norm
+from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs
+from scipy.stats import norm
+from numpy import mean
+from numpy import std
+
+# generate 2d classification dataset
+X, y = make_blobs(n_samples=100, centers=2, n_features=2, random_state=1)
+# summarize
+print(X.shape, y.shape)
 print(X[:5])
 print(y[:5])
+
+# fit a probability distribution to a univariate data sample
+def fit_distribution(data):
+	# estimate parameters
+	mu = mean(data)
+	sigma = std(data)
+	print(mu, sigma)
+	# fit distribution
+	dist = norm(mu, sigma)
+	return dist
+
+...
+# sort data into classes
+Xy0 = X[y == 0]
+Xy1 = X[y == 1]
+print(Xy0.shape, Xy1.shape)
+
+...
+# calculate priors
+priory0 = len(Xy0) / len(X)
+priory1 = len(Xy1) / len(X)
+print(priory0, priory1)
+
+...
+# create PDFs for y==0
+X1y0 = fit_distribution(Xy0[:, 0])
+X2y0 = fit_distribution(Xy0[:, 1])
+# create PDFs for y==1
+X1y1 = fit_distribution(Xy1[:, 0])
+X2y1 = fit_distribution(Xy1[:, 1])
+
+# summarize probability distributions of the dataset
+
+# fit a probability distribution to a univariate data sample
+
+
+# generate 2d classification dataset
+X, y = make_blobs(n_samples=100, centers=2, n_features=2, random_state=1)
+# sort data into classes
+Xy0 = X[y == 0]
+Xy1 = X[y == 1]
+print(Xy0.shape, Xy1.shape)
+# calculate priors
+priory0 = len(Xy0) / len(X)
+priory1 = len(Xy1) / len(X)
+print(priory0, priory1)
+# create PDFs for y==0
+X1y0 = fit_distribution(Xy0[:, 0])
+X2y0 = fit_distribution(Xy0[:, 1])
+# create PDFs for y==1
+X1y1 = fit_distribution(Xy1[:, 0])
+X2y1 = fit_distribution(Xy1[:, 1])
